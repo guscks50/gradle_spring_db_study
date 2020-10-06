483a109c3e6f6b1f69b27c76433f45fc541f235a
hyunchan <guscks50@naver.com>
2020-10-06 16:59:46 +0900

    푸시 완료

---
+++ java/gradle_spring_db_study/config/AppCtx.java
@@ -0,0 +1,13 @@
+package gradle_spring_db_study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = { "gradle_spring_db_study.spring","gradle_spring_db_study.spring.mapper" })
+@Import({ContextDataSource.class, ContextSqlSession.class})
+public class AppCtx {
+  
+}
+
